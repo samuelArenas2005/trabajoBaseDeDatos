3ec1852b741a5ae089ca7cc4dee477a0181a516e
SamuelArenas2005 <e.sir.samuel.arenas@cali.edu.co>
2025-04-29 21:03:26 -0500

    cambios

--- Scripts movefast/DQL.sql
+++ Scripts movefast/DQL.sql
@@ -20,26 +20,19 @@ WHERE CURRENT_DATE
 
 -- Calcular los ingresos totales por sucursal considerando solo vehículos con más de 3 alquileres.
 
-SELECT
-  s.*,
-  SUM(p.valor) AS ingresos_totales
-FROM sucursal s
-JOIN vehiculo v
-  ON v.sucursal = s.idsucursal
-JOIN (
-  SELECT
-    vehiculoPlaca
-  FROM alquiler
-  GROUP BY vehiculoPlaca
-  HAVING COUNT(*) > 3
-) AS v3
-  ON v.placa = v3.vehiculoPlaca
-JOIN alquiler a
-  ON a.vehiculoPlaca = v.placa
-JOIN pago p
-  ON p.idalquiler = a.idalquiler
-GROUP BY
-  s.idsucursal;
+SELECT s.ciudad, SUM(p.valor) AS ingresos_totales
+FROM pago p
+JOIN alquiler a ON p.idalquiler = a.idalquiler
+JOIN vehiculo v ON v.placa = a.vehiculoPlaca
+JOIN sucursal s ON v.sucursal = s.idsucursal
+WHERE v.placa IN (
+    SELECT vehiculoPlaca
+    FROM alquiler
+    GROUP BY vehiculoPlaca
+    HAVING COUNT(*) > 3
+)
+GROUP BY s.ciudad;
+
 
 --  Filtrar solo vehículos con más de 5 alquileres (usar subconsulta)
 
